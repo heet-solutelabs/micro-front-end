b6770431d76c8464d3d8c21b78411de23642d383
heet-solutelabs <heet.g@solutelabs.com>
2023-07-10 19:22:07 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./component/MarketingApp";
+
+function App() {
+  return (
+    <div>
+      App
+      <MarketingApp />
+    </div>
+  );
+}
+
+export default App;
